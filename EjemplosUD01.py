09249de0cb1894c1ca0021dd95deed03836bd575
Alumno <urquiza.gamig24@triana.salesianos.edu>
2025-09-25 09:01:40 +0200

    25 septiembre

---
+++ EjemplosUD01.py
@@ -0,0 +1,9 @@
+print(bool(1))
+
+str1 = 'Cadena con comilla simple'
+str2 = "Cadena con comilla doble"
+str3 = '''  
+    Hola a todos esto es un texto para practicar los tipos de cadenas de Python
+'''
+
+print(str3)
