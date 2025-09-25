b134b903724a9848b0aad38f96303f75e291fa61
MiguelUrquiza <miguelurquiza.contacto@gmail.com>
2025-09-25 21:07:39 +0200

    Prueba String

--- ejemplo1.py
+++ ejemplo1.py
@@ -2,7 +2,23 @@ total_population = 157503
 avg_temperature=16.8
 prueba = total_population
 
+print(str(True));
+print(int('10'))
 
+print(int('FA',16))
+
+print('Primera\nSegunda')
+print('Prueba \t Tabulador')
+print('Vamos a \'necesitar\' probar')
+text =r'prueba\ntext'
+print(text)
+
+
+msg1=("Hola")
+msg2=("Adiós")
+print(msg1,msg2)
+print(msg1,msg2,sep='|')
+print(msg1,msg2,end='!!\n')
 
 print(type(avg_temperature))
 
