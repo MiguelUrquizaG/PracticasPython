a0bb07d4ef4d4ed0464283868949deadc2a984be
Alumno <urquiza.gamig24@triana.salesianos.edu>
2025-09-29 08:58:12 +0200

    Ejercicios 4 y 5

---
+++ ejercicios/ejercicio04.py
@@ -0,0 +1,10 @@
+num1 = int(input("Introduzca el primer número: "))
+num2 = int(input("Introduzca el segundo número: "))
+
+if num1 >num2:
+    print(f'El número {num1} (que es el primer número introducido) es mayor que el {num2}.')
+elif num1 == num2:
+    print(f'Ambos números son iguales.')
+else:
+    print(f'El número {num2} (que es el segundo introducido) es mayor que el {num1}.')
+
