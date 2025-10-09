338b1f6ab958f9ceb8e4dd8d9c1601370402598c
Alumno <urquiza.gamig24@triana.salesianos.edu>
2025-10-09 10:03:43 +0200

    Clase

--- EjerciciosUD01MiguelUrquiza/ejercicio05.py
+++ EjerciciosUD01MiguelUrquiza/ejercicio05.py
@@ -1,2 +1,7 @@
 num1 = int(input("Introduzca un número: "))
 
+if num1 >= 0 and num1<=10:
+    print(f'El número está entre 0 y 10 y es: {num1}')
+else:
+    print(f'El número no está entre 0 y 10 y es: {num1}')
+
