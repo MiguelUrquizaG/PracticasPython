7e8d2fa1bacdb2bd0cb75c98696466041b09e191
Alumno <urquiza.gamig24@triana.salesianos.edu>
2025-10-10 12:07:22 +0200

    Repaso

--- EjerciciosUD01MiguelUrquiza/ejercicio14.py
+++ EjerciciosUD01MiguelUrquiza/ejercicio14.py
@@ -2,7 +2,7 @@
 Crea una función en python, triangulo, que reciba un número entero, e imprima un patrón como este por
 pantalla. Este sería el resultado de llamar a triangulo(5)
 '''
-
+'''
 print("Bienvenido, este programa genera un triángulo en base a la medida que le digas")
 
 triangulo = int(input("Introduce la longitud: "))
@@ -24,6 +24,23 @@ for i in range(0,triangulo):
 
 for j in range(triangulo,0,-1):
     print("*" *j)
+'''
+import math
+serial_number = '\n\t \n  Hola\t\n 48374983274832  \n\n\t \t \n'
 
+print(serial_number.strip('\n'))
 
+print(math.pow(3,2))
+age=16
+match age:
+    case j if j<17:
+        print("Menor")
+    case _ :
+        print("Mayor")
+
+texto ="Hola, me llamo Miguel, y soy programador"
+
+print(texto.split(","))
+'''
 print("Gracias por utilizar el programa")
+'''
