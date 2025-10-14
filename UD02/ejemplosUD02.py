92a26881eaa384a883ec5046043fbbc0cb1dd5fd
Alumno <urquiza.gamig24@triana.salesianos.edu>
2025-10-14 08:57:34 +0200

    Listas

---
+++ UD02/ejemplosUD02.py
@@ -0,0 +1,13 @@
+empty_list =[]
+languages = ["Python","Ruby","Javascript"]
+fibonacci = [0,1,1,2,3,5,8,21]
+data = ['Tenerife',{'cielo':'limpio','temp':24},3718,(28.2933947,-16.5226597)]
+ciudades = ["Sevilla","Edimburgo","Seúl","Tokio","Florencia"]
+
+print(ciudades[-1])
+#print(ciudades[1123112])
+print(ciudades[:3])
+print(ciudades[-1:-4:-1])
+
+palabra = 'Python'
+print(list(palabra))
