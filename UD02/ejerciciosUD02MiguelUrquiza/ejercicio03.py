a86296a615464dae7cb7ca6f31bc3330ac23bf96
MiguelUrquiza <miguelurquiza.contacto@gmail.com>
2025-10-23 23:08:22 +0200

    Ejercicio03

--- UD02/ejerciciosUD02MiguelUrquiza/ejercicio03.py
+++ UD02/ejerciciosUD02MiguelUrquiza/ejercicio03.py
@@ -8,6 +8,8 @@ maxVeces=5
 
 notasMaximas=[]
 cantNotasMaximas=0
+cantNotasMinimas=0
+suma=0
 
 while(contador<maxVeces):
     notaAnyadida=float(input("Introduzca una de sus notas: "))
@@ -26,5 +28,18 @@ for nota in notas:
     
 print(f'La nota máxima es: {numMax} y se repite {cantNotasMaximas} veces')
 
+listaMenor = sorted(notas,reverse=False)
+numMin = listaMenor[0]
+for nota in notas:
+    if nota == numMin:
+        cantNotasMinimas+=1
 
-print(notas)
+
+for nota in notas:
+    suma+=nota
+
+media = suma/len(notas)
+
+print(f'La nota mínima es: {numMin} y se repite {cantNotasMinimas} veces')
+print(f'La nota media es: {media}')
+
