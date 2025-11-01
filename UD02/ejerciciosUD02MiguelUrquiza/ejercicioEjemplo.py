a831953bab5d2fb686124edaf41168c5af7bfab3
MiguelUrquiza <miguelurquiza.contacto@gmail.com>
2025-11-01 22:36:59 +0100

    Ejercicio Angel

--- UD02/ejerciciosUD02MiguelUrquiza/ejercicioEjemplo.py
+++ UD02/ejerciciosUD02MiguelUrquiza/ejercicioEjemplo.py
@@ -14,6 +14,8 @@ edades = [22,16,43,10,63]
 nombres =["Miguel","Andrea","Raquel","Nerea","Juanma"]
 
 #Caso 1
+print("Caso 1")
+print("-----------------------------------------------------")
 media=0
 suma=0
 topeMedia=18
@@ -29,22 +31,59 @@ if media > topeMedia:
 else:
     print(f'La media de edad de los fallecidos es menor a 18, la media es: {media}')
 
-
+print("-----------------------------------------------------")
 #caso2
+print("Caso 2")
+print("-----------------------------------------------------")
 topeEdad=18
 textoCadena =' (Menor Edad)'
-for edad, nombre in zip(edades,nombres):
+for i,(edad, nombre) in  enumerate(zip(edades,nombres)):
     if edad < 18:
-        textoCadena.join(nombre) 
+        nombre = nombre + textoCadena
+        nombres[i] = nombre
+        print(nombre)
 
-for edad, nombre in zip(edades,nombres):
+for edad, nombre in zip(edades,nombres) :
     print(nombre, edad)
 
-
+print("-----------------------------------------------------")
 #caso3
+print("Caso 3")
+print("-----------------------------------------------------")
 maxLetras =5
 for nombre in nombres:
     if len(nombre )> maxLetras:
         print (nombre + ", tiene mas de 5 letras")
-
+print("-----------------------------------------------------")
 #caso4
+print("Caso 4")
+print("-----------------------------------------------------")
+sumaEdades=0
+for edad in edades:
+    if edad <18:
+        sumaEdades+=edad
+
+if sumaEdades > max(edades):
+    print("La suma de la edad de los menores es mayor que la del mas mayor")
+    print(f'Suma edades menores {sumaEdades}')
+    print(f'Edad más mayor: {max(edades)}')
+elif sumaEdades <max(edades):
+    print("La suma de la edad de los menores es menor que la del mas mayor")
+    print(f'Suma edades menores {sumaEdades}')
+    print(f'Edad más mayor: {max(edades)}')
+else:
+    print("Son iguales")
+    print(f'Suma edades menores {sumaEdades}')
+    print(f'Edad más mayor: {max(edades)}')
+
+print("-----------------------------------------------------")
+#caso 5
+print("Caso 5")
+print("-----------------------------------------------------")
+nombreBuscado = input("Introduzca el nombre del que quiere saber la edad: ")
+
+for nombre, edad in zip(nombres,edades):
+    if nombreBuscado == nombre:
+        print(f'El nombre es: {nombre}')
+        print(f'Su edad es: {edad}')
+        break
