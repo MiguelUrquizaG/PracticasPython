1ae36fe0586b4f168ae147690e1019fd7b8cbcca
MiguelUrquiza <miguelurquiza.contacto@gmail.com>
2025-11-10 19:14:55 +0100

    Ejercicio 2 List3

--- UD02/ejerciciosUD02List3MiguelUrquiza/ejercicio02.py
+++ UD02/ejerciciosUD02List3MiguelUrquiza/ejercicio02.py
@@ -1,13 +1,14 @@
 diccionario={
     'cabeza':'amigo',
     'aro':'circunferencia | claro, sí'
-             }
+    }
 
 clave=''
 valor=''
 palabraBuscada=''
 palabraModificar = ''
 opcion =-1
+palabrasFavoritas={}
 
 print('Bienvenido al programa')
 while opcion!=0:
@@ -16,8 +17,11 @@ while opcion!=0:
     print('2. Imprimir diccionario')
     print('3. Buscar palabra')
     print('4. Modificar palabra')
+    print('5. Añadir palabras favoritas al diccionario.')
+    print('6. Eliminar palabra')
+    print('7. Ordenar lista alfabéticamnte')
 
-    opcion = int(input('Introduzca lo que desea realizar'))
+    opcion = int(input('Introduzca lo que desea realizar: '))
 
     match opcion:
         case 0:
@@ -27,36 +31,37 @@ while opcion!=0:
             valor = input(f'Introduzca el significado de la palabra {clave}: ')
             diccionario[clave]=valor
         case 2:
-              for palabra, significado in diccionario.items():
+            for palabra, significado in diccionario.items():
                 print(f'{palabra} -> {significado}')
         case 3:
-             palabraBuscada = input('Introduzca la palabra de la cuál quiere saber el significado: ')
-             significadoPalabraBuscada =diccionario.get(palabraBuscada,'La palabra buscada no existe en el diccionario o está mal escrita.')
-             print(significadoPalabraBuscada)
+            palabraBuscada = input('Introduzca la palabra de la cuál quiere saber el significado: ')
+            significadoPalabraBuscada =diccionario.get(palabraBuscada,'La palabra buscada no existe en el diccionario o está mal escrita.')
+            print(significadoPalabraBuscada)
         case 4:
-             for palabra, significado in diccionario.items():
-                print(f'{palabra} -> {significado}')
-               
-             palabraBuscada = input('Introduzca la palabra a la cuál le desea cambiar el significado: ')
-             if palabraBuscada  not in diccionario:
+            for palabra, significado in diccionario.items():
+                print(f'{palabra}: {significado}')
+            
+            palabraBuscada = input('Introduzca la palabra a la cuál le desea cambiar el significado: ')
+            if palabraBuscada  not in diccionario:
                 print('Palabra no existente')
-             else:
+            else:
                 nuevoValor = input(f'Indica el nuevo significado que le quieres dar : ')
                 diccionario[palabraBuscada] = nuevoValor
                 for palabra, significado in diccionario.items():
-                    print(f'{palabra} -> {significado}')
-
-
-
-
-
-    
-
-   
-
-  
-
-   
-
-   
-
+                    print(f'{palabra}: {significado}')
+        case 5:
+            while len(palabrasFavoritas)<2:
+                clave = input('Introduzca la palabra que desea añadir: ')
+                valor = input('Introduzca el significado de esta palabra: ')
+                palabrasFavoritas[clave] = valor
+            
+            diccionario.update(palabrasFavoritas)
+        case 6:
+            valor = input('Introduzca la palabra que desee eliminar: ')
+            if valor in diccionario:
+                del diccionario[valor]
+            else:
+                print('Esa palabra no se encuentra en el diccionario.')
+        case 7:
+            for palabra,significado in sorted(diccionario.items()):
+                print(f'{palabra}: {significado}')
