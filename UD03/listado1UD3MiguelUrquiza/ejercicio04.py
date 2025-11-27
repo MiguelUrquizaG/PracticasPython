0b5ae4854b41d2965912db91c7e2e4cf1417eddf
Alumno <urquiza.gamig24@triana.salesianos.edu>
2025-11-27 10:44:45 +0100

    Adelanto Ejercicio4

--- UD03/listado1UD3MiguelUrquiza/ejercicio04.py
+++ UD03/listado1UD3MiguelUrquiza/ejercicio04.py
@@ -6,4 +6,10 @@ diagonal, como muestran los ejemplos siguientes:
 longitudDiagonales = int(input('Introduzca la longitud de la diagonal: '))
 
 def doble_Cuadrado(longitud):
-    
+    for _ in range(longitud):
+        print("*\t"*longitud+"\n")
+    for _ in range(longitud):
+        print("AA")
+
+
+doble_Cuadrado(longitudDiagonales)
