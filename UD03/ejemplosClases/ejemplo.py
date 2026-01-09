4a4f976c1084a9831706a122382a925a197e961f
Alumno <urquiza.gamig24@triana.salesianos.edu>
2026-01-09 14:10:41 +0100

    Clase

--- UD03/ejemplosClases/ejemplo.py
+++ UD03/ejemplosClases/ejemplo.py
@@ -1,7 +1,23 @@
 class StarWarsDroud:
-    pass
+    def switch_on(self):
+        self.power_on = True
+        print("Hi! I'm a droid.")
+    def switch_off(self):
+        self.power_on = False
+        print("I'm turned off.")
 
 c3po = StarWarsDroud()
 
-print(type(c3po))
+c3po.switch_on()
+print(c3po.power_on)
+
+#print(type(c3po))
+
+class Droid:
+ def __init__(self,name:str):
+    self.name =name
  
+
+prueba = Droid()
+
+#print(prueba.name)
