c9aa50fa009f4458218be48e8e1d3c48d7bd6f23
MiguelUrquiza <miguelurquiza.contacto@gmail.com>
2026-01-15 00:28:58 +0100

    Metodos Clases

--- UD03/ejemplosClases/ejemplosGestoresDeContextos.py
+++ UD03/ejemplosClases/ejemplosGestoresDeContextos.py
@@ -15,5 +15,34 @@ class Timer:
         print(f'El tiempo de ejecución fue: {exec_time}')
 
 with Timer():
-    for _ in range(1_000_000_000):
+    for _ in range(2_000_000):
         x =2**20
+
+
+class Droid:
+    def __init__(self,name:str):
+        self.name = name
+        self.covered_distance = 0
+    
+    def move_up(self,steps:int)->None:
+        self.covered_distance +=steps
+        print(f'Moving {steps} steps')
+
+#Preguntar porque esto no va si no uso init.
+class FrozenDroid():
+    def __init__(self,name:str):
+        self.name = name
+
+    def __enter__(self):
+        self.droid = Droid(self.name)
+        return self.droid
+    def __exit__(self,*err):
+        self.droid.covered_distance = 0
+
+
+with FrozenDroid("R2D2") as droid:
+    droid.move_up(10)
+    droid.move_up(50)
+    droid.move_up(-10)
+    print(droid.covered_distance)
+    
