a08600541916c0f6e1bc7b65fc69f2eff4e62a2a
Alumno <urquiza.gamig24@triana.salesianos.edu>
2026-01-15 09:00:52 +0100

    Herencia

---
+++ UD03/ejemplosHerencia/ejemplo.py
@@ -0,0 +1,10 @@
+#En Python se heredan tambien los constructores.
+#El super().loquesea se pone cuando defines el método
+#En Java esta prohibida la herencia múltiple pero en Python está permitido
+#Esto es heredar de más de una clase.
+#El orden de las clases de las que se heredan es importante 
+#Importa cuando tengamos que usar métodos que tengan el mismo nombre en clases de arriba.
+#Los mixins son como las interfaces, estas tienen cuerpo pero son herencia.
+
+#Hacer ejercicio ejemplo herencia ficheros apuntes.
+#Mirar estructuras mágicas
